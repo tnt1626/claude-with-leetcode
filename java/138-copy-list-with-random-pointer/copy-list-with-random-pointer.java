c35d6dbdb25793aded5897af566e5dd74083f8a6
Chi Vi Han <hanchivi39@gmail.com>
2026-06-13 16:54:02 +1000

    Update java/138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
    Co-authored-by: mirabile-cli[bot] <273517529+mirabile-cli[bot]@users.noreply.github.com>

--- java/138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ java/138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -12,7 +12,22 @@ class Node {
     }
 }
 */
+import java.util.HashMap;
+import java.util.Map;
 
+// Definition for a Node.
+// class Node {
+//     int val;
+//     Node next;
+//     Node random;
+//     public Node(int val) {
+//         this.val = val;
+//         this.next = null;
+//         this.random = null;
+//     }
+// }
+
+class Solution {
 class Solution {
 
     public Node copyRandomList(Node head) {
