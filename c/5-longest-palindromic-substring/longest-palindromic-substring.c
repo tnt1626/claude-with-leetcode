c7aabaaa124046f238233111b684a167f2a380ca
To Ngoc Tien <24280109@student.hcmus.edu.vn>
2026-06-17 00:22:37 +0700

    feat(c): add solution for 5-longest-palindromic-substring

---
+++ c/5-longest-palindromic-substring/longest-palindromic-substring.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+
+char* longestPalindrome(char* s) {
+    int n = strlen(s);
+
+    int index, maxLen = 0;
+    int l, r;
+    for(int i = 0; i < n; i++){
+        l = r = i;
+        while(r + 1 < n && s[r + 1] == s[r]){
+            r++;
+        }
+
+        while(l >= 0 && r < n && s[l] == s[r]){
+            if(r - l + 1 > maxLen){
+                maxLen = r - l + 1;
+                index = l;
+            }
+            l--;
+            r++;
+        }
+    }
+
+    char *result = (char *)malloc(sizeof(char) * (maxLen + 1));
+    strncpy(result, s + index, maxLen);
+    result[maxLen] = '\0';
+    return result;
+}
