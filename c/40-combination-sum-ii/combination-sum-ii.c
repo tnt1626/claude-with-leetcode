fcbd928b78e0e834307ab76c081cbf739572b675
To Ngoc Tien <24280109@student.hcmus.edu.vn>
2026-06-28 18:27:39 +0700

    feat(c): add solution for 40-combination-sum-ii

---
+++ c/40-combination-sum-ii/combination-sum-ii.c
@@ -0,0 +1,51 @@
+#define N 200
+
+int cmp(const void *a, const void *b){
+    return *(int *)a - *(int *)b;
+}
+
+void dfs(int* nums, int index, int size, int *temp, int *count, int target, int **result, int* rows, int** cols){
+
+    if(target == 0){
+        result[(*rows)] = (int *)malloc(sizeof(int) * (*count));
+        for(int j = 0; j < (*count); j++){
+            result[(*rows)][j] = temp[j];
+        }
+        (*cols)[(*rows)] = (*count);
+        (*rows)++;
+        return;
+    }
+
+    if(index >= size || target < 0){
+        return;
+    }
+
+    temp[(*count)++] = nums[index];
+    dfs(nums, index + 1, size, temp, count, target - nums[index], result, rows, cols);
+
+    (*count)--;
+    while(index + 1 < size && nums[index + 1] == nums[index]){
+        index++;
+    }
+    dfs(nums, index + 1, size, temp, count, target, result, rows, cols);
+}
+
+int** combinationSum2(int* candidates, int candidatesSize, int target, int* returnSize, int** returnColumnSizes) {
+
+    int **result = (int **)malloc(sizeof(int *) * N);
+    (*returnColumnSizes) = (int *)malloc(sizeof(int) * N);
+    *returnSize = 0;
+
+    int *temp = (int *)malloc(sizeof(int) * N);
+    int count = 0;
+
+    qsort(candidates, candidatesSize, sizeof(int), cmp);
+
+    dfs(candidates, 0, candidatesSize, temp, &count, target, result, returnSize, returnColumnSizes);
+
+    result = (int **)realloc(result, sizeof(int *) * (*returnSize));
+    (*returnColumnSizes) = (int *)realloc((*returnColumnSizes), sizeof(int) * (*returnSize));
+
+    free(temp);
+    return result;
+}
